24a2069a43ac662e601d67160e09db2cac0e3ce0
SoleSun <joel.ahn@alumni.ubc.ca>
2019-12-25 23:28:36 -0800

    Copied pong code. Got it to move in simple harmonic motion

--- GlassHouse.py
+++
@@ -1,8 +0,0 @@
-class GlassHouse(widget):
-    def GlassHouse(self, pos_x, pos_y):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        
-
-
-
--- main.py
+++ main.py
@@ -1,10 +1,59 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
+from kivy.vector import Vector
+from kivy.clock import Clock
+
 import GlassHouse
 
-class GlassHouseApp(app):
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+        self.velocity[1] -= 0.1
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def serve_ball(self, vel=(1, -2)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce ball off bottom or top
+        if (self.ball.y < self.y):
+            self.ball.velocity_y *= -1
+
+        if self.ball.x > self.x:
+        	self.ball.velocity_x *= -1
+
+class PongApp(App):
     def build(self):
-        return GlassHouse()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 2.0 / 60.0)
+        return game
+
 
 if __name__ == '__main__':
-    GlassHouseApp().run()
+    PongApp().run()
